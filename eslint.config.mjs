49c179bca2df5681ff014336a5ed411d18daebc8
Abhishek <manishp@301io.com>
2024-10-07 22:10:13 +0530

    feat: Setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  languageOptions: {
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  files: ["**/*.ts"],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  rules: {
+    "no-console": "error",
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
+  },
+});
